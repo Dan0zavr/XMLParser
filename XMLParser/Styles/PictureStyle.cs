470bfd2fab2d0552e984df904bf5d5d95c0ef189
Dan0zavr <dansemenogit@gmail.com>
2025-11-05 15:01:53 +0300

    Все лишнее удалено из индекса

--- XMLParser/Styles/PictureStyle.cs
+++ XMLParser/Styles/PictureStyle.cs
@@ -9,11 +9,12 @@ namespace XMLParser.Styles
     public class PictureStyle : ParagraphStyle, IStyle
     {
         public string StyleType => "PictureStyle";
-
-        public string PictureAllignment { get; set; }
-        public string LableAllignment { get; set; }
+        
         public bool AutoGenerateLable { get; set; }
-        public string? LabelValue { get; set; } 
-        public ParagraphStyle PictureParagraphStyle { get; set; }
+        public string? LabelValue { get; set; }
+        public string? LabelNumberingType { get; set; }
+        public bool EmptyLineBefore { get; set; }
+        public bool EmptyLineAfter { get; set; }
+
     }
 }
